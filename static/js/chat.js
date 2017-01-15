de8775854198eef450192d2f6569eeb3485af595
punkyoon <cryptography@konkuk.ac.kr>
2017-01-16 02:00:52 +0900

    added a files

---
+++ static/js/chat.js
@@ -0,0 +1,5 @@
+$(function() {
+    var ws_scheme = window.location.protocol == 'https:' ? 'wss' : 'ws';
+    // var ws_path = ws_scheme + '://' + window.location.host + 'temp';
+    // var socket = new ReconnectingWebSocket(ws_path);
+})
