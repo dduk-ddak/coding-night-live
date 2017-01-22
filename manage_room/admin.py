6eaa99d99c12430ea7acfc4fac1176eca917ee16
punkyoon <cryptography@konkuk.ac.kr>
2017-01-23 01:26:09 +0900

    can manage a room in admin site

--- manage_room/admin.py
+++ manage_room/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Room
 
 # Register your models here.
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('admin_user', 'title', 'link', 'time', 'label')
+
+admin.site.register(Room, RoomAdmin)
--- manage_room/models.py
+++ manage_room/models.py
@@ -4,21 +4,28 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-from channels import Group
+import allauth
+
+#from channels import Group
 
 # Create your models here.
+
 class Room(models.Model):
-    admin_user = models.ForeignKey(User, on_delete=True)    #fk
-    email = admin_user.email
+    admin_user = models.ForeignKey(User, on_delete=models.CASCADE)  #fk
+    #admin_user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    #admin_user = models.ForeignKey(allauth.socialaccount.models.SocialAccount, on_delete=models.CASCADE)
+    
+    #email = admin_user.email
     title = models.CharField(max_length=255, default="NoTitle")
     link = models.URLField(primary_key=True)    #pk
-    time = models.DateTimeField(default=timezone.now, db_index=True)
+    time = models.DateTimeField(default=timezone.now)
     #slide = models.Field()  #fk
 
     label = models.SlugField(unique=True)
 
     def __str__(self):
         return self.title
+
 """
 class Slide(models.Model):
     md_blob = models.FileField()
