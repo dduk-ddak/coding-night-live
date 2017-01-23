29f0663df15b2cf3d5956487ba4a2eff9a16fe96
Ubuntu <punk@punkServer.tskf3tbunefulletentev2tlee.lx.internal.cloudapp.net>
2017-01-23 06:27:38 +0000

    create a chatting model

--- manage_chat/models.py
+++ manage_chat/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+
+from manage_room.models import Room
 
 # Create your models here.
+class Notice(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    _id = models.AutoField(primary_key=True)
+    time = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+
+class Poll(models.Model):
+    _id = models.AutoField(primary_key=True)
+    #slide #fk
+    time = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+
+class ChatAndReply(models.Model):
+    _id = models.AutoField(primary_key=True)
+    #slide #fk
+    hash_value = models.CharField(default=_createHash, unique=True)
+    time = models.DateTimeField(default=timezone.now)
+    is_reply = models.BooleanField(default=False)
+    description = models.TextField()
+
