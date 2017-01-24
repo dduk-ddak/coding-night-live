dd9b681bdd65f82cab79322709e7f222943e47b5
punkyoon <cryptography@konkuk.ac.kr>
2017-01-24 15:28:13 +0900

    added a _createHash function in manage_chat/models.py

--- manage_chat/models.py
+++ manage_chat/models.py
@@ -1,9 +1,21 @@
+import os
+import time
+import hashlib
+from os import path
+from binascii import hexlify
+
 from django.db import models
 from django.utils import timezone
 
 from manage_room.models import Room
 
 # Create your models here.
+def _createHash():
+    """This function generate 10 character long hash"""
+    hash = hashlib.sha1()
+    hash.update(str(time.time()))
+    return  hash.hexdigest()[:-10]
+
 class Notice(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     _id = models.AutoField(primary_key=True)
@@ -19,7 +31,7 @@ class Poll(models.Model):
 class ChatAndReply(models.Model):
     _id = models.AutoField(primary_key=True)
     #slide #fk
-    hash_value = models.CharField(default=_createHash, unique=True)
+    hash_value = models.CharField(max_length=10, default=_createHash, unique=True)
     time = models.DateTimeField(default=timezone.now)
     is_reply = models.BooleanField(default=False)
     description = models.TextField()
