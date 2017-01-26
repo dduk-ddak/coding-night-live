8e177e4488260867d57807c8308e0cb4dfbbdf54
punkyoon <cryptography@konkuk.ac.kr>
2017-01-26 22:46:31 +0900

    fixed a ChatAndReply model's hash error

--- manage_chat/admin.py
+++ manage_chat/admin.py
@@ -6,12 +6,10 @@ class NoticeAdmin(admin.ModelAdmin):
     list_display = ('room', '_id', 'time', 'description')
 
 class PollAdmin(admin.ModelAdmin):
-    list_display = ('_id', 'time', 'description')
-    #list_display = ('slide', '_id', 'time', 'description')
+    list_display = ('room', '_id', 'time', 'description')
 
 class ChatAndReplyAdmin(admin.ModelAdmin):
-    list_display = ('_id', 'time', 'description', 'hash_value', 'is_reply')
-    #list_display = ('slide', '_id', 'time', 'description', 'hash_value', 'is_reply')
+    list_display = ('room', '_id', 'time', 'description', 'hash_value', 'is_reply')
 
 admin.site.register(Notice, NoticeAdmin)
 admin.site.register(Poll, PollAdmin)
--- manage_chat/models.py
+++ manage_chat/models.py
@@ -1,21 +1,17 @@
-import os
 import time
 import hashlib
-from os import path
-from binascii import hexlify
 
 from django.db import models
 from django.utils import timezone
 
 from manage_room.models import Room
-#from manage_room.models import Room, Slide
 
 # Create your models here.
 def _createHash():
     """generate 10 character long hash"""
-    hash = hashlib.sha1()
-    hash.update(str(time.time()))
-    return  hash.hexdigest()[:-10]
+    now = str(time.time()).encode('utf-8')
+    hash = hashlib.sha1(now)
+    return hash.hexdigest()[:-10]
 
 class Notice(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)    #fk
@@ -26,13 +22,9 @@ class Notice(models.Model):
     def __str__(self):
         return self._id
 
-    def __str__(self):
-        return self._id
-
 class Poll(models.Model):
     _id = models.AutoField(primary_key=True)
-    #room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    #slide = models.ForeignKey(Slide, on_delete=models.CASCADE)  #fk
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     time = models.DateTimeField(default=timezone.now)
     description = models.TextField()
 
@@ -41,8 +33,7 @@ class Poll(models.Model):
 
 class ChatAndReply(models.Model):
     _id = models.AutoField(primary_key=True)
-    #slide = models.ForeignKey(Slide, on_delete=models.CASCADE)  #fk
-    #room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     hash_value = models.CharField(max_length=10, default=_createHash, unique=True)
     time = models.DateTimeField(default=timezone.now)
     is_reply = models.BooleanField(default=False)
