7aed1bff628be6127da26769dd74012960b33349
punkyoon <cryptography@konkuk.ac.kr>
2017-01-27 05:45:50 +0000

    modified a regex

--- coding_night_live/urls.py
+++ coding_night_live/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^services/$', manage_room.views.RoomListView, name='services'),
     url(r'^services/new/', manage_room.views.RoomCreateView, name='new'),
-    url(r'^services/delete/(?P<pk>([a-z]{3,}-[a-z]{3,}-[0-9]{4}))/$', manage_room.views.RoomDeleteView, name='delete'),
-    url(r'^([a-z]{3,}-[a-z]{3,}-[0-9]{4})/$', manage_room.views.RedirectRoomView.as_view(), name='redirect_room'),
+    url(r'^services/delete/(?P<pk>([a-z]{3,}-[a-z]{3,}-[0-9]{1,4}))/$', manage_room.views.RoomDeleteView, name='delete'),
+    url(r'^([a-z]{3,}-[a-z]{3,}-[0-9]{1,4})/$', manage_room.views.RedirectRoomView.as_view(), name='redirect_room'),
     url(r'^auth/', include('django.contrib.auth.urls'), {'next_page': '/'}),
 ]
