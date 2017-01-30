dc8993b8956201654242e5f9fd7d165566486a1f
punkyoon <cryptography@konkuk.ac.kr>
2017-01-30 18:58:05 +0900

    rename ; /room/stream/ to /room/user_count/

--- coding_night_live/routing.py
+++ coding_night_live/routing.py
@@ -1,6 +1,6 @@
 from channels import include
 
 channel_routing = [
-    include("manage_room.routing.websocket_routing", path=r"^/room/stream"),
+    include("manage_room.routing.websocket_routing", path=r"^/room/user_count/"),
     include("manage_room.routing.custom_routing"),
 ]
