0ea7d24c3b171658a1627f21947eb254ee19b4f4
punkyoon <cryptography@konkuk.ac.kr>
2017-02-01 05:55:50 +0000

    solved issue #19 - logged in user redirection

--- coding_night_live/views.py
+++ coding_night_live/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
 
 # Create your views here.
 class MainView(TemplateView):
     template_name='main.html'
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseRedirect('/services/')
+        return super(MainView, self).dispatch(request, *args, **kwargs)
