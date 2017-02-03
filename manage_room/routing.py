d193aee1b17138b2fefed3cde9aa37cdd1c7fe66
punkyoon <cryptography@konkuk.ac.kr>
2017-02-04 02:47:29 +0900

    added a routing for using consumers

--- manage_room/routing.py
+++ manage_room/routing.py
@@ -1,18 +1,14 @@
 from channels import route
-from .consumers import room_join, room_leave, room_title_rename
-#from .consumers import ws_connect, ws_receive, ws_disconnect, room_join, room_leave
-
-"""
-websocket_routing = [
-    route("websocket.connect", ws_connect),
-    route("websocket.receive", ws_receive),
-    route("websocket.ws_disconnect", ws_disconnect),
-]
-"""
+from .consumers import room_join, room_leave, room_title_rename, new_slide, del_slide, change_slide_order, rename_slide, get_slide_list
 
 # Websocket command : join => call room_join
 custom_routing = [
     route("room.receive", room_join, command="^join$"),
     route("room.receive", room_leave, command="^leave"),
     route("room.receive", room_title_rename, command="^rename_room_title$"),
+    route("room.receive", new_slide, command="^new_slide$"),
+    route("room.receive", del_slide, command="^del_slide$"),
+    route("room.receive", change_slide_order, command="^change_order$"),
+    route("room.receive", rename_slide, command="^rename_slide$"),
+    route("room.receive", get_slide_list, command="^get_slide_list$"),
 ]
