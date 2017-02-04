0716db4e8a3ebaf09047318be86a236978a10d31
Kwanghee Choi <juice500ml@gmail.com>
2017-02-04 11:02:34 +0000

    globals namespace defined.

---
+++ static/js/cnl_globals_override.js
@@ -0,0 +1,25 @@
+// simplemde (left div: editing text area)
+cnl_globals.editor = new SimpleMDE({
+  element: document.getElementById("code"),
+  autofocus: true,
+  spellChecker: true,
+  forceSync: true,
+  lineNumbers: true,
+  toolbarTips: false,
+  promptURLs: false,
+  insertTexts: {
+    image: ["![enter image description here](http://", ")"],
+    link: ["[enter link description here](http://", ")"],
+    horizontalRule: ["", "\n\n-----\n\n"],
+    table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
+  },
+  toolbar: ["bold", "italic", "strikethrough", "heading", "|",
+  "code", "quote", "horizontal-rule", "|",
+  "ordered-list", "unordered-list", "table", "|",
+  "link", "image", "|",
+  "guide"],
+}).codemirror.on("change", function(e) {
+  var out = document.getElementById("out");
+  out.innerHTML = cnl_globals.md.render(e.getValue());
+  $('blockquote').addClass('blockquote');
+});
