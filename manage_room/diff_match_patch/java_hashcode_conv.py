4035df48b1fae141abc1cd8bd7508c5c4b20b556
Kwanghee Choi <juice500ml@gmail.com>
2017-02-05 16:04:47 +0000

    int and c_int32 is different. javascript's h | 0 meant int->signed 32bit conversion.

--- manage_room/diff_match_patch/java_hashcode_conv.py
+++ manage_room/diff_match_patch/java_hashcode_conv.py
@@ -1,9 +1,10 @@
+from ctypes import c_int32
+
 def javaHash(s):
-    max_h = (1<<32)
     h = 0
     if not s: return 0
     for ch in s:
         ch = ord(ch)
         h = ((h<<5)-h) + ch
-        h %= max_h
+        h = c_int32(h).value
     return h
