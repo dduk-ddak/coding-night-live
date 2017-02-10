4c06f23b855346fc1f1469270d1453a8af2eb51a
punkyoon <cryptography@konkuk.ac.kr>
2017-02-10 19:11:38 +0900

    setting db timezone

--- coding_night_live/settings.py
+++ coding_night_live/settings.py
@@ -154,7 +154,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+#USE_TZ = True
+USE_TZ = False
 
 # django social auth setting ; login redirect and logout url
 LOGIN_REDIRECT_URL = "/services/"
