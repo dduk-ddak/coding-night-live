528a90858b1297a81b97b3e26568f2460ac9e89d
punkyoon <cryptography@konkuk.ac.kr>
2017-02-10 21:27:31 +0900

    reconnect rename_room function

--- manage_room/routing.py
+++ manage_room/routing.py
@@ -5,8 +5,9 @@ from .consumers import room_join, room_leave, new_slide, del_slide, change_slide
 custom_routing = [
     route("room.receive", room_join, command="^join$"),
     route("room.receive", room_leave, command="^leave$"),
+    route("room.receive", rename_room, command="^rename_room$"),
     route("room.receive", new_slide, command="^new_slide$"),
-    route("room.receive", get_slide, command="^get_slide$"), 
+    route("room.receive", get_slide, command="^get_slide$"),
     route("room.receive", del_slide, command="^del_slide$"),
     route("room.receive", curr_slide, command="^curr_slide$"),
     route("room.receive", change_slide_order, command="^change_slide_order$"),
