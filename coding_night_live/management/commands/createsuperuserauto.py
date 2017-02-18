c240f5424f4ca45f3ba7bb9df8dc3a9200de4a87
punkyoon <cryptography@konkuk.ac.kr>
2017-02-18 15:22:25 +0000

    modified space

--- coding_night_live/management/commands/createsuperuserauto.py
+++ coding_night_live/management/commands/createsuperuserauto.py
@@ -2,12 +2,11 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.utils.crypto import get_random_string
 
-
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         password = get_random_string()
-        print('Generated root password: %s' % (password,))
+        print('Generated root password : %s' % (password,))
         admin = User.objects.create_superuser(
             email='root@localhost',
             username='root',
@@ -16,3 +15,4 @@ class Command(BaseCommand):
         admin.is_active = True
         admin.is_admin = True
         admin.save()
+
