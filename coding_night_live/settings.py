5d1ad47e1f9367326d0e881dda0ece2118b77957
punkyoon <cryptography@konkuk.ac.kr>
2017-03-23 04:48:10 +0000

    option recovery - DEBUG=True to DEBUG=False

--- coding_night_live/settings.py
+++ coding_night_live/settings.py
@@ -42,8 +42,7 @@ def get_secret(setting, secret=secret):
 SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
