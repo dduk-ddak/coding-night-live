012938b853991320b2d4d83178aa01109e84642b
Minho Ryang <minhoryang@gmail.com>
2017-04-11 22:32:35 +0900

    [Issue #83][Enhancement] Merging and extracting variables

--- postgres/init_sql.sh
+++ postgres/init_sql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --dbname "$POSTGRES_DB" <<-EOSQL
     ALTER ROLE $POSTGRES_USER SET client_encoding TO 'utf8';
     ALTER ROLE $POSTGRES_USER SET default_transaction_isolation TO 'read committed';
     ALTER ROLE $POSTGRES_USER SET timezone TO 'UTC-9';
